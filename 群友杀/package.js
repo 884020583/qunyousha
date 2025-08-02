b16927096db0b9bf79b79ca4863ad9e4607c7e7b
884020583 <153368100+884020583@users.noreply.github.com>
2025-08-03 01:18:46 +0800

    Add files via upload

--- 群友杀/package.js
+++ 群友杀/package.js
@@ -1,9 +1,10 @@
-extension["群友杀"]={
-	intro:"灵光乍现和超绝行动力的产物",
-	author:"云笺",
-	netdisk:"无",
-	forum:"QQ",
-	version:"1.0",
-	files:["info.json","README.md","怜魚.jpg","KK.jpg","狐黎.jpg","测试用角色.jpg","LICENSE","info.json","KK.jpg","LICENSE","package.js","README.md","怜魚.jpg","测试用角色.jpg","狐黎.jpg"],
-	size:"5.4MB"
+extension["群友杀"] = {
+	date: "2025/08/02",
+	intro: "灵光一闪和超绝行动力的产物(云师傅工作量好大啊，来个人救救云师傅吧喵)",
+	author: "云笺",
+	netdisk: "无",
+	forum: "QQ群",
+	version: "1.0",
+	files: ["extension.js", "info.json", "KK酱.jpg", "LICENSE", "package.js", "README.md", "测试用角色.jpg", "狐黎.jpg", "怜魚.jpg"],
+	size: "5.4MB"
 };
