10b780a0266fda68323bd10f3beb6399c70b7b81
884020583 <153368100+884020583@users.noreply.github.com>
2025-08-04 12:36:27 +0800

    Update catalog.js

--- catalog.js
+++ catalog.js
@@ -4,15 +4,17 @@ if (!localStorage.getItem('tip')) {
 };
 
 extension["群友杀"] = {
-	date: "2025/08/03",
+	date: "2025/08/04",
 	intro: "灵光一闪和超绝行动力的产物(云师傅工作量好大啊，来个人救救云师傅吧喵)",
 	author: "云笺",
 	netdisk: "https://qm.qq.com/q/DKBfxgqP4G",
 	forum: "https://qm.qq.com/q/DKBfxgqP4G",
 	version: "1.0",
-	files: ["extension.js", "character.js", "characterSkill.js", "info.json",
-		"KK酱.jpg", "LICENSE", "package.js", "README.md", "测试用角色.jpg", "狐黎.jpg", "怜魚.jpg",
+	files: ["extension.js", "character.js", "characterSkill.js", "info.json", "LICENSE", "README.md",
+
 		"global/helpFunction.js",
+
+		"KK酱.jpg", "package.js", "测试用角色.jpg", "狐黎.jpg", "怜魚.jpg", "Censored.jpg",
 	],
-	size: "2.54MB"
+	size: "2.6MB"
 };
